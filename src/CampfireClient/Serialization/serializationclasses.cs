3644b8ec29fef1a4c79c347a8ab1496cd36719c0
Arild Fines <arild.fines@broadpark.no>
2011-09-05 21:29:41 +0200

    Added a specs project and a first serialization class

---
+++ src/CampfireClient/Serialization/serializationclasses.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Rogue.MetroFire.CampfireClient.Serialization
+{
+	public class Account
+	{
+		[XmlElement("id")]
+		public int Id { get; set; }
+
+		[XmlElement("name")]
+		public string Name { get; set; }
+		
+		[XmlElement("subdomain")]
+		public string Subdomain { get; set; }
+
+		[XmlElement("plan")]
+		public string Plan { get; set; }
+
+		[XmlElement("storage")]
+		public string Storage { get; set; }
+		
+		[XmlElement("created-at")]
+		public DateTime CreatedAt { get; set; }
+
+		[XmlElement("updated-at")]
+		public DateTime UpdatedAt { get; set; }
+	}
+}

e69017d4cbc7e799c7288a3450940b4cb635418e
Arild Fines <arild.fines@broadpark.no>
2011-10-10 21:43:49 +0200

    Make match texts take regular expressions

--- src/MetroFire/Notifications/Trigger.cs
+++ src/MetroFire/Notifications/Trigger.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Rogue.MetroFire.CampfireClient.Serialization;
 using Rogue.MetroFire.UI.Settings;
 using Rogue.MetroFire.CampfireClient;
@@ -8,6 +10,58 @@ namespace Rogue.MetroFire.UI.Notifications
 	public class Trigger
 	{
 		private readonly NotificationTrigger _notificationTrigger;
+
+		private readonly Regex _textRegex;
+
+		private readonly Regex _userRegex;
+
+		private readonly Regex _roomRegex;
+
+		public Trigger(NotificationTrigger notificationTrigger)
+		{
+			_notificationTrigger = notificationTrigger;
+			_userRegex = CreateRegex(_notificationTrigger.MatchUser);
+			_roomRegex = CreateRegex(_notificationTrigger.MatchRoom);
+			_textRegex = CreateRegex(_notificationTrigger.MatchText);
+		}
+
+		private Regex CreateRegex(string regexText)
+		{
+			return new Regex(regexText ?? "", RegexOptions.IgnoreCase);
+		}
+
+		public bool Matches(NotificationMessage notificationMessage)
+		{
+			if (!IsMatchingType(notificationMessage.Message.Type))
+			{
+				return false;
+			}
+			var misMatches = from m in new[]
+				{
+					new {Regex = _roomRegex, Text = notificationMessage.Room.Name},
+					new {Regex = _userRegex, Text = notificationMessage.User.Name},
+					new {Regex = _textRegex, Text = notificationMessage.Message.Body}
+				}
+				where !m.Regex.IsMatch(m.Text)
+				select m;
+
+			if (misMatches.Any())
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		private bool IsMatchingType(MessageType type)
+		{
+			MessageType[] types;
+			if (!TriggerMatches.TryGetValue(_notificationTrigger.TriggerType, out types))
+			{
+				return false;
+			}
+			return type.In(types);
+		}
 
 		private static readonly Dictionary<TriggerType, MessageType[]> TriggerMatches =
 			new Dictionary<TriggerType, MessageType[]>
@@ -28,49 +82,5 @@ namespace Rogue.MetroFire.UI.Notifications
 					{TriggerType.UserMessage, new[] {MessageType.PasteMessage, MessageType.TextMessage, MessageType.UploadMessage}},
 					{TriggerType.UserPaste, new[] {MessageType.PasteMessage, MessageType.UploadMessage}}
 				};
-
-		public Trigger(NotificationTrigger notificationTrigger)
-		{
-			_notificationTrigger = notificationTrigger;
-
-
-		}
-
-		public bool Matches(NotificationMessage notificationMessage)
-		{
-			if (!IsMatchingType(notificationMessage.Message.Type))
-			{
-				return false;
-			}
-			if (_notificationTrigger.MatchRoom != null && 
-				!notificationMessage.Room.Name.ToLower().Contains(_notificationTrigger.MatchRoom.ToLower()))
-			{
-				return false;
-			}
-
-			if (_notificationTrigger.MatchUser != null &&
-				!notificationMessage.User.Name.ToLower().Contains(_notificationTrigger.MatchUser.ToLower()))
-			{
-				return false;
-			}
-
-			if (_notificationTrigger.MatchText != null &&
-				!notificationMessage.Message.Body.ToLower().Contains(_notificationTrigger.MatchText.ToLower()))
-			{
-				return false;
-			}
-
-			return true;
-		}
-
-		private bool IsMatchingType(MessageType type)
-		{
-			MessageType[] types;
-			if (!TriggerMatches.TryGetValue(_notificationTrigger.TriggerType, out types))
-			{
-				return false;
-			}
-			return type.In(types);
-		}
 	}
 }

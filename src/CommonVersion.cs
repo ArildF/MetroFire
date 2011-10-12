ff08f7766abf8ff4212d60a693903d3f87abb107
Arild Fines <arild.fines@broadpark.no>
2011-10-12 08:24:31 +0200

    Bump versions.

--- src/CommonVersion.cs
+++ src/CommonVersion.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyFileVersion("0.2.0.0")]

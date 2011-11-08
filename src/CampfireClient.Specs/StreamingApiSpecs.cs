d36f01228cfe0152fab5bc83df593fc6c4948ef7
Arild Fines <arild.fines@broadpark.no>
2011-11-08 22:19:56 +0100

    Fix API specs

--- src/CampfireClient.Specs/StreamingApiSpecs.cs
+++ src/CampfireClient.Specs/StreamingApiSpecs.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Reactive.Subjects;
 using System.Threading;
 using Machine.Specifications;
 using Rogue.MetroFire.CampfireClient.Serialization;
@@ -12,7 +13,8 @@ namespace Rogue.MetroFire.CampfireClient.Specs
 		Establish context = () =>
 			{
 				_id = FindJoinedRoom();
-
+				var subject = new Subject<ConnectionState>();
+				subject.Subscribe(cs => _connectionStates.Add(cs));
 				_disposable = api.Stream(_id, msg =>
 					{
 						_messages.Add(msg);
@@ -20,7 +22,7 @@ namespace Rogue.MetroFire.CampfireClient.Specs
 						{
 							_event.Set();
 						}
-					});
+					}, subject);
 
 				_msg = @"Hello
 world " + Guid.NewGuid();
@@ -39,11 +41,14 @@ world " + Guid.NewGuid();
 		It should_have_proper_created_date = () => _messages.ShouldEachConformTo(msg => msg.CreatedAt != DateTime.MinValue);
 		It should_have_room_id = () => _messages.ShouldEachConformTo(msg => msg.RoomId > 0);
 
+		It should_have_reported_streaming_connected = () => _connectionStates.ShouldContain(cs => cs.Connected);
+
 		private static readonly AutoResetEvent _event = new AutoResetEvent(false);
 
 		private static readonly List<Message> _messages = new List<Message>();
 		private static int _id;
 		private static IDisposable _disposable;
 		private static string _msg;
+		private static readonly List<ConnectionState> _connectionStates = new List<ConnectionState>();
 	}
 }

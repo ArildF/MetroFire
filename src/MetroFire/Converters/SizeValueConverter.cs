9be1ef50aebae15ccec8913674b5bd3bab8ad4a4
Arild Fines <arild.fines@broadpark.no>
2012-05-18 15:14:40 +0200

    Use designtime background brush (stolen from the Core project)

---
+++ src/MetroFire/Converters/SizeValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Rogue.MetroFire.UI.Converters
+{
+	public class SizeValueConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var val = (long) value;
+
+			if (val > 1.MegaBytes())
+			{
+				return val.AsMegaBytes() + "MB";
+			}
+			return val.AsKiloBytes() + "KB";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/MetroFire/Infrastructure/DesignTime.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Rogue.MetroFire.UI.Infrastructure
+{
+	public static class DesignTime
+	{
+		private static readonly Brush DefaultBackgroundBrush =
+			new SolidColorBrush(Colors.DarkGray);
+
+		public static Brush GetBackground(DependencyObject obj)
+		{
+			return (Brush) obj.GetValue(BackgroundProperty);
+		}
+
+		public static void SetBackground(DependencyObject obj, Brush value)
+		{
+			obj.SetValue(BackgroundProperty, value);
+		}
+
+		public static readonly DependencyProperty BackgroundProperty = DependencyProperty.RegisterAttached(
+			"Background",
+			typeof (Brush),
+			typeof (DesignTime),
+			new PropertyMetadata(
+				DefaultBackgroundBrush,
+				OnBackgroundPropertyChanged));
+
+		private static void OnBackgroundPropertyChanged(
+			DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			if (!DesignerProperties.GetIsInDesignMode(d))
+				return;
+
+			var p = d as Panel;
+			if (p != null)
+				p.Background = e.NewValue as Brush;
+
+			var c = d as Control;
+			if (c != null)
+			{
+				c.Background = e.NewValue as Brush;
+			}
+		}
+
+	}
+}

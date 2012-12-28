528567af9aab7f242e02fe46cac51f30b9a17cbf
Arild Fines <arild.fines@broadpark.no>
2012-12-28 21:57:51 +0100

    Prevent non-numeric values for max backlog

---
+++ src/MetroFire/Behaviors/RestrictToNumericalInputBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using System.Linq;
+
+namespace Rogue.MetroFire.UI.Behaviors
+{
+	public class RestrictToNumericalInputBehavior : Behavior<TextBox>
+	{
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+
+			AssociatedObject.PreviewTextInput += AssociatedObjectOnPreviewTextInput;
+
+		}
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+			AssociatedObject.PreviewTextInput -= AssociatedObjectOnPreviewTextInput;
+		}
+
+
+		private void AssociatedObjectOnPreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			if (!e.Text.All(Char.IsDigit))
+			{
+				e.Handled = true;
+			}
+		}
+	}
+}

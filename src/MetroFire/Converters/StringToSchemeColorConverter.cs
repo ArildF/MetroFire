82adcf61c3d7b79897651c5ed89c934380ab983f
Arild Fines <arild.fines@broadpark.no>
2013-05-22 22:18:54 +0200

    Use a variety of colors on the room buttons in the lobby

---
+++ src/MetroFire/Converters/StringToSchemeColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Rogue.MetroFire.UI.Converters
+{
+	public class StringToSchemeColorConverter : IValueConverter
+	{
+		private static readonly Color[] SchemeColors = new[]
+			{
+				Color.FromRgb(25, 203, 255),
+				Color.FromRgb(0, 110, 141),
+				Color.FromRgb(0, 169, 218),
+				Color.FromRgb(141, 70, 0),
+				Color.FromRgb(218, 108, 0)
+			};
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			uint hashCode = (uint)value.GetHashCode();
+			var index = hashCode % SchemeColors.Length;
+
+			return SchemeColors[index];
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

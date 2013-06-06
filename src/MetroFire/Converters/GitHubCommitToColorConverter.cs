d2e29ebed22327f481252a952a237f04595be0cb
Arild Fines <arild.fines@broadpark.no>
2013-06-06 22:05:30 +0200

    Indicate changes not in the current version in the "What's new" list.

---
+++ src/MetroFire/Converters/GitHubCommitToColorConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Windows.Data;
+using System.Windows.Media;
+using Rogue.MetroFire.UI.GitHub;
+
+namespace Rogue.MetroFire.UI.Converters
+{
+	public class GitHubCommitToColorConverter : IValueConverter
+	{
+		private readonly DateTime _assemblyVersionTimeStamp;
+
+		private static readonly DateTime Epoch = new DateTime(2012,07,17);
+
+		public GitHubCommitToColorConverter()
+		{
+			var version = Assembly.GetExecutingAssembly().GetName().Version;
+			var timeSpan = TimeSpan.FromHours(version.Build);
+			_assemblyVersionTimeStamp = Epoch + timeSpan;
+
+		}
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var commit = (Commit) value;
+
+			if (commit.CommitDetails.Author.Date > _assemblyVersionTimeStamp)
+			{
+				return Brushes.LightGoldenrodYellow;
+			}
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

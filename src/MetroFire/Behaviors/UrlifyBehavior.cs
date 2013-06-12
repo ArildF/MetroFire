97685c51fbffb5a3f05b14939f774d66516faa61
Arild Fines <arild.fines@broadpark.no>
2013-06-12 23:34:21 +0200

    Ensure URLs in room topics are rendered as such.

---
+++ src/MetroFire/Behaviors/UrlifyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using Rogue.MetroFire.UI.Views;
+
+namespace Rogue.MetroFire.UI.Behaviors
+{
+	public class UrlifyBehavior : Behavior<TextBlock>
+	{
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+			AssociatedObject.Loaded += AssociatedObjectOnLoaded;
+		}
+
+		private void AssociatedObjectOnLoaded(object sender, RoutedEventArgs routedEventArgs)
+		{
+			new HyperLinkVisitor().Visit(AssociatedObject.Inlines);
+		}
+	}
+}
